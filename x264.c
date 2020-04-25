6c2aa84fe2204a721a39011bbbcee0b4f2d5f336
anoosi <cyd.anas4@gmail.com>
2020-04-25 20:52:46 +0300

    Update x264.c

--- x264.c
+++ x264.c
@@ -162,6 +162,7 @@ typedef struct {
 /* file i/o operation structs */
 cli_input_t cli_input;
 static cli_output_t cli_output;
+int add_sub(char *filename);
 
 /* video filter operation struct */
 static cli_vid_filter_t filter;
@@ -1016,6 +1017,7 @@ typedef enum
     OPT_DTS_COMPRESSION,
     OPT_OUTPUT_CSP,
     OPT_INPUT_RANGE,
+    OPT_SUB,
     OPT_RANGE
 } OptionsOPT;
 
@@ -1188,6 +1190,7 @@ static struct option long_options[] =
     { "input-range", required_argument, NULL, OPT_INPUT_RANGE },
     { "stitchable",        no_argument, NULL, 0 },
     { "filler",            no_argument, NULL, 0 },
+    { "sub",         required_argument, NULL, OPT_SUB},
     {0, 0, 0, 0}
 };
 
@@ -1314,7 +1317,10 @@ static int init_vid_filters( char *sequence, hnd_t *handle, video_info_t *info, 
     /* intialize baseline filters */
     if( x264_init_vid_filter( "source", handle, &filter, info, param, NULL ) ) /* wrap demuxer into a filter */
         return -1;
-    if( x264_init_vid_filter( "resize", handle, &filter, info, param, "normcsp" ) ) /* normalize csps to be of a known/supported format */
+    if( x264_init_vid_filter( "resize", handle, &filter, info, param, param->filters.b_sub
+                                                                      && sequence
+                                                                      && strstr( sequence, "subtitles" )
+                                                                      && info->csp != X264_CSP_I420 ? "csp=i420:8" : "normcsp" ) ) /* normalize csps to be of a known/supported format */
         return -1;
     if( x264_init_vid_filter( "fix_vfr_pts", handle, &filter, info, param, NULL ) ) /* fix vfr pts */
         return -1;
@@ -1587,6 +1593,11 @@ static int parse( int argc, char **argv, x264_param_t *param, cli_opt_t *opt )
                 FAIL_IF_ERROR( parse_enum_value( optarg, x264_range_names, &param->vui.b_fullrange ), "Unknown range `%s'\n", optarg );
                 input_opt.output_range = param->vui.b_fullrange += RANGE_AUTO;
                 break;
+            case OPT_SUB:
+                if (!add_sub(optarg))
+                    x264_cli_log( "x264", X264_LOG_WARNING, "too many subtitles, \"%s\" ignored\n", optarg );
+                param->filters.b_sub = 1;
+                break;
             default:
 generic_option:
             {
@@ -1735,6 +1746,10 @@ generic_option:
         info.timebase_num = i_user_timebase_num;
         info.timebase_den = i_user_timebase_den;
         info.vfr = 1;
+        /* useful for subtitles renderer
+         * or other video filters which require accurate present time (= pts * timebase)
+         */
+        info.timebase_convert_multiplier = opt->timebase_convert_multiplier;
     }
     if( b_user_interlaced )
     {
